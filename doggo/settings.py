c2254129402491c3ecf55fccf5959a1893bdb3a7
cenergy <673598118@qq.com>
2021-05-15 13:17:28 +0800

    add helloD2 dev code

--- doggo/settings.py
+++ doggo/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import sys
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,18 +30,45 @@ SECRET_KEY = 'django-insecure-oa0m%=m-$gcpc9ripnwg=9+qy*^t0av1kc2_dx+f)1na_e*g^1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
+# 跨域
+CORS_ORIGIN_ALLOW_ALL = True
 
+AUTH_USER_MODEL = 'users.UserProfile'
+
+# --------------------自定义认证后端-------------------------------------
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.weibo.WeiboOAuth2',
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.qq.QQOAuth2',
+    'social_core.backends.weixin.WeixinOAuth2',
+    'users.views.CustomBackend',
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 以上是默认的apps
+    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
+    'ais.apps.AisConfig',
+    'courses.apps.CoursesConfig',
+    'sources.apps.SourcesConfig',
+    'wechat.apps.WechatConfig',
+    # 以下是第三方apps
+    'django_filters',
+    'social_django',
+    'captcha',
+    'rest_framework',
+    'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +86,7 @@ ROOT_URLCONF = 'doggo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +94,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',  # 新增
+                'social_django.context_processors.login_redirect',  # 新增
             ],
         },
     },
@@ -79,6 +113,28 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# 连接MySQL数据库
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'geo_users',
+#         'USER': 'root',
+#         'PASSWORD':'Cenergy',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
+# 连接postgreSQL数据库
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'aigisss',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres.neng',
+#         'HOST': '127.0.0.1',
+#         'PORT': '9555',
+#     }
+# }
 
 
 # Password validation
@@ -103,23 +159,105 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 下面是自定义的一些
+
+
+# 指定simpleui默认的主题,指定一个文件名，相对路径就从simpleui的theme目录读取
+SIMPLEUI_DEFAULT_THEME = 'admin.lte.css'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.AutoSchema',
+    # "DEFAULT_VERSIONING_CLASS": 'rest_framework.versioning.URLPathVersioning',
+    # "ALLOWED_VERSIONS": ['v1', "v2"],
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '10000/day'
+    }
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': datetime.timedelta(days=1),
+}
+
+# 邮箱设置
+EMAIL_HOST = "smtp.sina.com"
+EMAIL_PORT = 25
+EMAIL_HOST_USER = "helloaigis@sina.com"
+EMAIL_HOST_PASSWORD = "Cenergy.0919"
+EMAIL_USE_TLS = False
+EMAIL_FROM = "helloaigis@sina.com"
+
+# ----------------------手机号码正则表达式-------------------------------
+REGEX_MOBILE = "^1[358]\d{9}$|^147\d{8}$|^176\d{8}$"
+# 云片网apikey 配置
+API_KEY = "09d6805265afbd0a779cfd9e16a0f4c5"
+
+# 百度识别# 定义常量
+BAIDU_APP_ID = '11800206'
+BAIDU_API_KEY = 'sAy8l7GrgGMBfesVoPkYtr0m'
+BAIDU_SECRET_KEY = 'Ex4Yitab1ZTq8y3FykTpa3kbGvpfUvjV'
+
+# 百度地图
+BAIDU_MAP_KEY = 'tDM947ZCUIZXzs7ohNHsz77QkU22WzDa'
+
+# 图灵api
+TURING_API_KEY = 'bf61c090a1bc4cfabc43e20e2d5b307b'
--- doggo/urls.py
+++ doggo/urls.py
@@ -16,6 +16,49 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import url, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.authtoken import views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="AIGIS's API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+handler404 = 'users.views.page_not_found_view'
+handler500 = 'users.views.server_error_view'
+handle403 = 'users.views.permission_denied_view'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url(r'^ais/', include('ais.urls')),
+    url(r'^api/', include('api.urls')),
+    url(r'^courses/', include('courses.urls')),
+    url(r'^sources/', include('sources.urls')),
+    url(r'^wechat', include('wechat.urls')),
+    #drf自带的token认证模式
+    url(r'^api-token-auth/', views.obtain_auth_token),
+    url('^accounts/', include('social_django.urls', namespace='social')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    url(r'^', include('users.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+
