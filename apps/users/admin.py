22bab464d3a51d82c8d37e142970a2e3ddade8f2
cenergy <673598118@qq.com>
2021-05-18 12:56:56 +0800

    modify admin admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from import_export.admin import ExportMixin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from .models import UserProfile,EmailVerifyRecord
-
 @admin.register(UserProfile)
-class UserProfileAdmin(ExportMixin,admin.ModelAdmin):
-    list_display = ('username','email')
+class UserProfileAdmin(ExportMixin,UserAdmin):
     list_per_page = 20
+    list_display = ('username','last_login','is_superuser','is_staff','is_active','date_joined')
+
 @admin.register(EmailVerifyRecord)
 class EmailVerifyAdmin(ExportMixin,admin.ModelAdmin):
     list_display = ('email','send_time')
