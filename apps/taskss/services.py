017e84e13262a52151f9945a26e48d50800bb3e0
cenergy <673598118@qq.com>
2024-05-28 21:36:16 +0800

    修改定时任务不行的问题

--- apps/taskss/services.py
+++ apps/taskss/services.py
@@ -1,4 +1,5 @@
 from django.db import connection
+from django.test.client import Client
 
 
 import os, sys,time,datetime,requests
@@ -8,6 +9,8 @@ from doggo.settings import MEDIA_ROOT,STATIC_ROOT
 # 打开文件
 path = MEDIA_ROOT+"/recognition/images/"
 dirs = os.listdir(path)
+# 创建一个测试客户端实例
+client = Client()
  
 # 输出所有文件和文件夹
 aDayTicks = 60*60*24*1000
@@ -24,7 +27,7 @@ def delRegImage():
             os.remove(f)
 
 def getGalleryCache():
-    requests.get('https://api.aigisss.com/resources/galleryCache/')
+    response = client.get('/resources/galleryCache/', {'key': 'value'})
 
 def getGithub():
-    requests.get('https://api.aigisss.com/api/github/cenergy')
+    response = client.get('/api/github/cenergy', {'key': 'value'})
